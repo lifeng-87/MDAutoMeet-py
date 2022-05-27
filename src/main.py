e694c3092cd3e98424777d16c62dd5cda2425001
lifeng87 <oscarcoll.930714@gmail.com>
2022-05-28 00:11:37 +0800

    firefox

--- src/main.py
+++ src/main.py
@@ -1,29 +1,19 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 options = Options()
-options.add_argument("start-maximized")
-options.add_argument("--disable-infobars")
-options.add_experimental_option("detach", True)
-options.add_experimental_option("prefs", { \
-    "profile.default_content_setting_values.media_stream_mic": 1, 
-    "profile.default_content_setting_values.media_stream_camera": 1,
-    "profile.default_content_setting_values.geolocation": 1, 
-    "profile.default_content_setting_values.notifications": 1 
-})
+options.set_preference("permissions.default.microphone",1)
+options.set_preference("permissions.default.camera",1)
 
-browser = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()), options=options)
-browser.get('https://accounts.google.com/signin/v2/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2Fu%2F0%2Fh&followup=https%3A%2F%2Fclassroom.google.com%2Fu%2F0%2Fh&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+browser = webdriver.Chrome(executable_path=f"{os.getcwd()}/web_driver/geckodriver.exe", options=options)
+browser.get('https://accounts.google.com/signin/v2/identifier?ltmpl=meet&continue=https%3A%2F%2Fmeet.google.com%3Fhs%3D193&&o_ref=https%3A%2F%2Fwww.google.com%2F&_ga=2.155881595.1533375318.1653442791-696588692.1653442791&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
 
 WebDriverWait(browser, 2000).until(
     EC.presence_of_element_located((By.ID, "identifierId"))
@@ -44,3 +34,5 @@ passwd.send_keys(os.getenv("PASSWD"))
 
 next = browser.find_element(By.XPATH, '//*[@id="passwordNext"]/div/button')
 next.click()
+
+sleep(2)
